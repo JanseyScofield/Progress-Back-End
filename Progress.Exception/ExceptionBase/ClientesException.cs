b9783040ee3e521d3a3d01da91c50587bcb4d71e
JanseyScofield <janscofield10@gmail.com>
2025-03-05 10:48:29 -0300

    Criando a classe de ClientesException

---
+++ Progress.Exception/ExceptionBase/ClientesException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progress.Exception.ExceptionBase
+{
+    public class ClientesException : ArgumentException
+    {
+        public ClientesException(string message) : base(message)
+        {
+
+        }
+    }
+}
