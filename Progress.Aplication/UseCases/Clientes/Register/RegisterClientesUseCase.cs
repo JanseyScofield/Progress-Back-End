78f2c69ac7e0e6b2de679464cd6d6cd05176c081
JanseyScofield <janscofield10@gmail.com>
2025-03-12 09:56:28 -0300

    Realocando  o método de validar CNPJ de para uma classe Utils

--- Progress.Aplication/UseCases/Clientes/Register/RegisterClientesUseCase.cs
+++ Progress.Aplication/UseCases/Clientes/Register/RegisterClientesUseCase.cs
@@ -40,7 +40,9 @@ namespace Progress.Aplication.UseCases.Clientes.Register
 
         private void Validate(RequestRegisterClienteJson request)
         {
-            if (!ValidarCNPJ(request.CNPJ))
+            var utils = new Utils();
+
+            if (!utils.ValidarCNPJ(request.CNPJ))
             {
                 throw new ClientesException(ResourceErrorMessages.CNPJ_INVALIDO);
             }
@@ -81,60 +83,5 @@ namespace Progress.Aplication.UseCases.Clientes.Register
             }
         }
 
-        private bool ValidarCNPJ(string CNPJ)
-        {
-            if (CNPJ.Length != 14)
-            {
-                return false;
-            }
-            if (CNPJ.All(d => d == CNPJ[0]))
-            {
-                return false;
-            }
-
-            const int constanteCalculo = 11; 
-            int[] arrayMult = {6, 5, 4, 3, 2, 9, 8, 7}; 
-            int soma = 0, restoDiv;
-            int iCont, jCont;
-            int primeiroDigito, segundoDigito;
-
-            jCont = 1;
-            for(iCont = 0; iCont < (CNPJ.Length - 2); iCont++)
-            {
-                soma += (CNPJ[iCont] - '0') * arrayMult[jCont];
-                jCont++;
-
-                if (jCont >= arrayMult.Length)
-                {
-                    jCont = 0;
-                }
-            }
-
-            restoDiv = soma % constanteCalculo;
-            primeiroDigito = restoDiv < 2 ? 0 : constanteCalculo - restoDiv;
-
-            if (primeiroDigito != (CNPJ[12] - '0'))
-            {
-                return false;
-            }
-
-            soma = jCont = 0;
-
-            for (iCont = 0; iCont < (CNPJ.Length - 1); iCont++)
-            {
-                soma += (CNPJ[iCont] - '0') * arrayMult[jCont];
-                jCont++;
-
-                if (jCont >= arrayMult.Length)
-                {
-                    jCont = 0;
-                }
-            }
-
-            restoDiv = soma % constanteCalculo;
-            segundoDigito = restoDiv < 2 ? 0 : constanteCalculo - restoDiv;
-
-            return segundoDigito == (CNPJ[13] - '0');
-        }
     }
 }
---
+++ Progress.Aplication/UseCases/Utils.cs
@@ -0,0 +1,61 @@
+﻿namespace Progress.Aplication.UseCases
+{
+    public class Utils
+    {
+        public bool ValidarCNPJ(string CNPJ)
+        {
+            if (CNPJ.Length != 14)
+            {
+                return false;
+            }
+            if (CNPJ.All(d => d == CNPJ[0]))
+            {
+                return false;
+            }
+
+            const int constanteCalculo = 11;
+            int[] arrayMult = { 6, 5, 4, 3, 2, 9, 8, 7 };
+            int soma = 0, restoDiv;
+            int iCont, jCont;
+            int primeiroDigito, segundoDigito;
+
+            jCont = 1;
+            for (iCont = 0; iCont < (CNPJ.Length - 2); iCont++)
+            {
+                soma += (CNPJ[iCont] - '0') * arrayMult[jCont];
+                jCont++;
+
+                if (jCont >= arrayMult.Length)
+                {
+                    jCont = 0;
+                }
+            }
+
+            restoDiv = soma % constanteCalculo;
+            primeiroDigito = restoDiv < 2 ? 0 : constanteCalculo - restoDiv;
+
+            if (primeiroDigito != (CNPJ[12] - '0'))
+            {
+                return false;
+            }
+
+            soma = jCont = 0;
+
+            for (iCont = 0; iCont < (CNPJ.Length - 1); iCont++)
+            {
+                soma += (CNPJ[iCont] - '0') * arrayMult[jCont];
+                jCont++;
+
+                if (jCont >= arrayMult.Length)
+                {
+                    jCont = 0;
+                }
+            }
+
+            restoDiv = soma % constanteCalculo;
+            segundoDigito = restoDiv < 2 ? 0 : constanteCalculo - restoDiv;
+
+            return segundoDigito == (CNPJ[13] - '0');
+        }
+    }
+}
