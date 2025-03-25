d9772f59b4426c132812a9ef9387911a8ce35472
JanseyScofield <janscofield10@gmail.com>
2025-03-25 15:07:04 -0300

    Adicionando tabela de Produtos na infrastrutura do projeto

---
+++ Progress.Infrastructure/Entitites/Produtos.cs
@@ -0,0 +1,10 @@
+﻿namespace Progress.Infrastructure.Entitites
+{
+    public class Produtos
+    {
+
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
