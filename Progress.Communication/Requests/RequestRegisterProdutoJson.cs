9a7093996f4c0a6bce5935e34662010171172dfe
JanseyScofield <janscofield10@gmail.com>
2025-03-25 15:20:07 -0300

    Adicioando as classes para requeste de registrar produtos e a response detalhada de produtos

---
+++ Progress.Communication/Requests/RequestRegisterProdutoJson.cs
@@ -0,0 +1,9 @@
+﻿namespace Progress.Communication.Requests
+{
+    public class RequestRegisterProdutoJson
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ Progress.Communication/Responses/ResponseProdutoDetailJson.cs
@@ -0,0 +1,9 @@
+﻿namespace Progress.Communication.Responses
+{
+    public class ResponseProdutoDetailJson
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
